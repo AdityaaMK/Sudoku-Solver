e3ed144a546a0611e2dbddeb4bb54027a736be6c
AdityaaMK <44482076+AdityaaMK@users.noreply.github.com>
2020-03-30 15:37:40 -0400

    User can input any sudoku puzzle to solve.

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,19 +1,6 @@
-sudoku_board = [
-    [0, 7, 0, 8, 0, 3, 0, 5, 0],
-    [5, 0, 0, 0, 1, 0, 2, 0, 3],
-    [0, 0, 3, 6, 0, 0, 8, 0, 0],
-    [0, 0, 0, 0, 0, 0, 6, 8, 4],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [9, 3, 4, 0, 0, 0, 0, 0, 0],
-    [0, 0, 7, 0, 0, 1, 3, 0, 0],
-    [8, 0, 6, 0, 2, 0, 0, 0, 1],
-    [0, 1, 0, 5, 0, 6, 0, 7, 0]
-]
-
-
 def computer_solve(bo):
-    display_board(sudoku_board)
-    print("\n\n\n")
+    # display_board(bo)
+    # print("\n\n\n")
     empty = get_empty(bo)
     if not empty:
         return False
@@ -22,8 +9,8 @@ def computer_solve(bo):
 
     # backtracking algorithm
     for i in range(1, 10):
-        if legal(bo, i, (row, col)):
-            print(row, col)
+        if is_legal(bo, i, (row, col)):
+            # print(row, col)
             bo[row][col] = i  # adds into board
 
             if not computer_solve(bo):  # recursively try to finish solution with new board
@@ -34,7 +21,7 @@ def computer_solve(bo):
     return True
 
 
-def legal(bo, num, pos):
+def is_legal(bo, num, pos):
     # Checking if row is valid
     for i in range(len(bo[0])):
         # first part of if statement checks if there is a duplicate of num and second part ignores the spot where we
@@ -85,7 +72,69 @@ def get_empty(bo):
                 return i, j  # returns row, column
 
 
-display_board(sudoku_board)
-computer_solve(sudoku_board)
-print("\n\n\n")
-display_board(sudoku_board)
+def is_valid(bo):
+    # Checks that all rows contain no repeats
+    for row in bo:
+        found = []
+        for i in row:
+            if i in found:
+                return False
+            if i != 0:
+                found.append(i)
+
+    # Checks that all columns contain no repeats
+    for c in range(9):
+        found = []
+        for r in range(9):
+            i = bo[r][c]
+            if i in found:
+                return False
+            if i != 0:
+                found.append(i)
+
+    return True
+
+
+grid = []
+
+if len(grid) == 0:
+    temp_grid = []
+    board = ""
+    while len(board) != 81:
+        temp_grid = []
+        board = input("Type in the board, going left to right row by row, 0 = empty: ")
+        if len(board) != 81:
+            print("Not 81 characters.")
+        else:
+            for row in range(9):
+                r = []
+                for col in range(9):
+                    r.append(int(board[row * 9 + col]))  # getting each value in the row
+                temp_grid.append(r)
+            if not is_valid(temp_grid):
+                data = ""
+                print("Please enter a valid board.")
+
+    grid = temp_grid
+
+if is_valid(grid):
+    print(grid)
+    display_board(grid)
+    computer_solve(grid)
+    print("\n\n\n")
+    display_board(grid)
+
+# sample_board = [
+#     [0, 7, 0, 8, 0, 3, 0, 5, 0],
+#     [5, 0, 0, 0, 1, 0, 2, 0, 3],
+#     [0, 0, 3, 6, 0, 0, 8, 0, 0],
+#     [0, 0, 0, 0, 0, 0, 6, 8, 4],
+#     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+#     [9, 3, 4, 0, 0, 0, 0, 0, 0],
+#     [0, 0, 7, 0, 0, 1, 3, 0, 0],
+#     [8, 0, 6, 0, 2, 0, 0, 0, 1],
+#     [0, 1, 0, 5, 0, 6, 0, 7, 0]
+# ]
+# print(sample_board)
+#
+
